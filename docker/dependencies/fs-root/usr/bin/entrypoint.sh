e75f656a31be0cc69cd8d3fd616da14e9b7d13df
Guo Du <mrduguo@gmail.com>
2025-03-25 08:45:07 +0000

    Remove pnpm build from entrypoint

--- docker/dependencies/fs-root/usr/bin/entrypoint.sh
+++ docker/dependencies/fs-root/usr/bin/entrypoint.sh
@@ -19,7 +19,7 @@ fi
 # part inside the "{}" is a workaround for the following bug in ash/dash:
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
-  pnpm build
+  # pnpm run init
   set -- pnpm exec reactiac "$@"
 fi
 
