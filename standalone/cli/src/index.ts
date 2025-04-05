48468373fe1c3007dad38ec3c5177357012d0b38
Guo Du <mrduguo@gmail.com>
2025-04-05 17:19:19 +0100

    Add debug message for cli start/finish

--- standalone/cli/src/index.ts
+++ standalone/cli/src/index.ts
@@ -4,8 +4,11 @@ import commands from "./commands/index.ts";
 import { OPTIONS_SYMBOL } from "./types.ts";
 import { updateCheck } from "./utils/updateCheck.ts";
 import { loadConfig } from "./utils/loadConfig.ts";
-
+import Debug from "debug";
+const debug = Debug("reactiac");
 setupDebug();
+debug("started at %O", new Date());
+
 await loadConfig();
 await updateCheck(true);
 
@@ -16,3 +19,5 @@ await runCommand({
   commands,
   options: commands[OPTIONS_SYMBOL],
 });
+
+debug("finished at %O", new Date());
