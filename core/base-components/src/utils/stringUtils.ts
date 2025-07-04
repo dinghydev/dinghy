83fb6050dfc4fa1aebdc0f209e4027123638e3b1
Guo Du <mrduguo@gmail.com>
2025-07-04 08:51:23 +0100

    Move words to stringUtils

--- core/base-components/src/utils/stringUtils.ts
+++ core/base-components/src/utils/stringUtils.ts
@@ -1,5 +1,3 @@
-import { capitalise } from './words.ts'
-
 export const renderKey = (str: string) =>
   str.startsWith('__') ? str.slice(2) : str
 
@@ -12,3 +10,32 @@ export const toName = (str: string) =>
 
 export const toTitle = (str: string) =>
   str.split(/\W/g).map(capitalise).join(' ')
+
+export const decapitalise = (str: string) => str[0].toLowerCase() + str.slice(1)
+export const capitalise = (str: string) => str[0].toUpperCase() + str.slice(1)
+
+export const camelCaseToWords = (str: string, spacer: string = ' ') => {
+  try {
+    return capitalise(str).match(/[A-Z][a-z0-9_]*/g)!.reduce(
+      (acc: string[], c) => {
+        if (acc.length == 0 || c.length > 1) {
+          acc.push(c)
+        } else {
+          const lastWord = acc.pop()
+          const lastChar = lastWord!.charAt(lastWord!.length - 1)
+          if (lastChar >= 'A' && lastChar <= 'Z') {
+            acc.push(lastWord + c)
+          } else {
+            acc.push(lastWord!)
+            acc.push(c)
+          }
+        }
+        return acc
+      },
+      [],
+    ).join(spacer)
+  } catch (e) {
+    console.error('Error capitalising', str)
+    throw e
+  }
+}
