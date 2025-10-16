1223ca85d43f0d830871e488897333fa333b0f62
Guo Du <mrduguo@gmail.com>
2025-10-16 20:13:06 +0100

    Update release site to use site argument

--- tasks/src/release-site.ts
+++ tasks/src/release-site.ts
@@ -2,15 +2,15 @@ import { execa } from 'execa'
 
 if (import.meta.main) {
   console.log(`Releasing site for ${Deno.args[0]}...`)
-  const siteDir = `./../sites/${Deno.args[0]}`
+  const site = Deno.args[0]
   await execa({
     stderr: 'inherit',
     stdout: 'inherit',
     input: `
       set -e
       cd ../engine/
-      deno run -A src/index.ts site build --app-home ${siteDir}
-      deno run -A src/index.ts site deploy --app-home ${siteDir}
+      deno run -A src/index.ts site build --app-home ./../ --site ${site}
+      deno run -A src/index.ts site deploy --app-home ./../ --site ${site}
     `,
   })`sh`
   console.log('Released site for', Deno.args[0])
