2436e14522ebb4f2e593d5b5c1cce051ad3ca9c9
Guo Du <mrduguo@gmail.com>
2025-12-31 16:16:34 +0000

    Open same site link in same window

---
+++ sites/www/src/theme/MDXComponents/A.tsx
@@ -0,0 +1,19 @@
+// Generated with:
+// dinghy site docusaurus -- swizzle --list
+// dinghy site docusaurus -- swizzle @docusaurus/theme-classic MDXComponents/A --danger --eject --typescript
+import React, { type ReactNode } from 'react'
+import Link from '@docusaurus/Link'
+import type { Props } from '@theme/MDXComponents/A'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+
+export default function MDXA(props: Props): ReactNode {
+  const { siteConfig } = useDocusaurusContext()
+  if (props.href.startsWith('https://')) {
+    const siteHostname = new URL(siteConfig.url).hostname
+    const hostname = new URL(props.href).hostname
+    if (hostname === siteHostname) {
+      props = { ...props, target: '_self' }
+    }
+  }
+  return <Link {...props} />
+}
